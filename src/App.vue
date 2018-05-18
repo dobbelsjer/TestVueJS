eafee1e5865a0f1f6a33faafe6abbc8ee2eaeabe
Jerome Dobbels <jerome.dobbels@epitech.eu>
2018-05-18 13:53:07 +0200

    CRD sans U

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,35 @@
 <template>
   <div id="app">
+    <p><button @click.prevent="getTv()">Actualiser</button></p>
     <form>
-      <p><label for="tvid">Tv Id: </label><input class="champ" type="text" id="tvid" ref="tvid"/></p>
       <p><label for="tvname">Tv name: </label><input class="champ" type="text" id="tvname" ref="tvname"/></p>
-      <p><label for="ip">Tv ip: </label><input class="champ" type="text" id="ip" ref="ip"/></p>
+      <p><label for="ipAdress">Tv Ip: </label><input class="champ" type="text" id="ipAdress" ref="ipAdress"/></p>
       <p><label for="compo">Tv composition: </label><input class="champ" type="text" id="compo" ref="compo"/></p>
-      <p><button @click.prevent="addTv()">Ajouter</button></p>
+      <p><button @click.prevent="postTv()">Ajouter</button></p>
     </form>
     <ul>
-      <li v-for="tvline in this.Tvs">{{ tvline.name }} | {{ tvline.ip }} | {{ tvline.composition_id }} <button @click.prevent="delTv(tvline)">Supprimer</button></li>
+      <li v-for="tv in this.Tvs">{{ tv.name }} | {{ tv.ipAdress }} | {{ tv.compositionId }} <button @click.prevent="delTv(tv)">Supprimer</button></li>
     </ul>
   </div>
 </template>
 
 <script>
-
+    var axios = require('axios');
 export default {
   name: 'App',
     data() {
       return {
-          Tvs: [
-              {id: '1', name: 'Tv1', ip: '192.168.1.25', composition_id: '1001'},
-              {id: '2', name: 'Tv2', ip: '192.168.1.26', composition_id: '1015'},
-              {id: '3', name: 'Tv3', ip: '192.168.1.27', composition_id: '1001'}
-          ],
-          tvline:{id:'', name:'', ip:'', composition_id:''}
+          Tvs: this.getTv(),
+          tv:{id:'', name:'', ipAdress:'', compositionId:''}
       }
     },
     methods: {
-        addTv() {
-            if(this.$refs.tvid.value == ''){
-                alert("Tv id can not be empty");
-                return false;
-            }else if (this.$refs.tvname.value == '' ){
+      addTv(){
+            if (this.$refs.tvname.value == '' ){
                 alert("Tv name can not be empty");
                 return false;
-            }else if (this.$refs.ip.value == '' ){
-                alert("Tv ip can not be empty");
+            }else if (this.$refs.ipAdress.value == '' ){
+                alert("Tv Ip can not be empty");
                 return false;
             }else if ( this.$refs.compo.value == ''){
                 alert("composition id can not be empty");
@@ -45,13 +38,42 @@ export default {
             this.Tvs.push({
                 id: this.$refs.tvid.value,
                 name: this.$refs.tvname.value,
-                ip: this.$refs.ip.value,
-                composition_id: this.$refs.compo.value
+                ipAdress: this.$refs.ipAdress.value,
+                compositionId: this.$refs.compo.value
             })
             return true;
         },
-        delTv: function(tvline){
-            this.Tvs.splice(this.Tvs.indexOf(tvline), 1)
+        delTv: function(tv){
+          axios.delete('http://localhost:8089/tv/' + tv.id)
+          this.Tvs.splice(this.Tvs.indexOf(tv), 1);
+        },
+        getTv(){
+          axios.get('http://localhost:8089/tv')
+              .then(response=>{
+                  console.log(response.data);
+                  this.Tvs = response.data;
+            });
+        },
+        postTv(){
+            if (this.$refs.tvname.value == '' ){
+                alert("Tv name can not be empty");
+                return false;
+            }else if (this.$refs.ipAdress.value == '' ){
+                alert("Tv Ip can not be empty");
+                return false;
+            }
+
+            this.tv.ipAdress = this.$refs.ipAdress.value;
+            this.tv.name = this.$refs.tvname.value;
+            this.tv.compositionId = this.$refs.compo.value;
+
+            axios.post('http://localhost:8089/tv', this.tv)
+                .then(response =>{
+                    console.log(response.data);
+                    console.log(response.data.name);
+                    console.log(response.data.compositionId);
+                    this.Tvs.push(response.data);
+                });
         }
     }
 }
